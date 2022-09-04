3006897b6df23cfd91ce72302c03eeb7d0c1fd0c
shota <contact@shota.dev>
2022-09-04 17:49:24 +0200

    Update viewer

--- src/main/java/dev/shota/decompiler/window/viewer/Viewer.java
+++ src/main/java/dev/shota/decompiler/window/viewer/Viewer.java
@@ -1,5 +1,6 @@
 package dev.shota.decompiler.window.viewer;
 
+import com.formdev.flatlaf.util.SystemInfo;
 import dev.shota.decompiler.reflection.Singleton;
 import javafx.scene.control.TabPane;
 
@@ -8,6 +9,9 @@ public class Viewer extends TabPane {
 
     private Viewer() {
         setMinWidth(300);
+        setTabClosingPolicy(TabClosingPolicy.ALL_TABS);
+        if (!(SystemInfo.isMacOS && SystemInfo.isMacFullWindowContentSupported))
+            setTabDragPolicy(TabDragPolicy.REORDER);
     }
 
 }
