96358e3b88c4abbbfd4f9224ff4e81093ffebee7
Sota <contact@hohltier.com>
2023-07-16 19:42:46 +0200

    Add procyon

--- src/main/java/com/hohltier/decompiler/transformers/ProcyonTransformer.java
+++ src/main/java/com/hohltier/decompiler/transformers/ProcyonTransformer.java
@@ -1,29 +1,23 @@
 package com.hohltier.decompiler.transformers;
 
 import com.hohltier.decompiler.models.FileModel;
-import com.strobel.assembler.metadata.Buffer;
-import com.strobel.assembler.metadata.ITypeLoader;
+import com.strobel.assembler.metadata.ArrayTypeLoader;
 import com.strobel.decompiler.Decompiler;
 import com.strobel.decompiler.DecompilerSettings;
 import com.strobel.decompiler.ITextOutput;
 import com.strobel.decompiler.PlainTextOutput;
 import org.jetbrains.annotations.NotNull;
 
-public class ProcyonTransformer implements ITransformer, ITypeLoader {
+public class ProcyonTransformer implements ITransformer {
 
     @Override
     public String transform(@NotNull FileModel fileModel) {
         DecompilerSettings settings = DecompilerSettings.javaDefaults();
-        settings.setTypeLoader(this);
+        ArrayTypeLoader typeLoader = new ArrayTypeLoader(fileModel.getBytes());
+        settings.setTypeLoader(typeLoader);
         ITextOutput output = new PlainTextOutput();
-        Decompiler.decompile(fileModel.getName(), output, settings);
+        Decompiler.decompile(typeLoader.getClassNameFromArray(), output, settings);
         return output.toString();
-    }
-
-    @Override
-    public boolean tryLoadType(String internalName, Buffer buffer) {
-        System.out.println("TODO");
-        return false;
     }
 
 }
