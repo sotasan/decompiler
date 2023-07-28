a9ba94ff3ce30146bd9c6d4bb436bb79400b0ae7
Sota <contact@hohltier.com>
2023-07-28 17:32:19 +0200

    Update about titlebar

--- src/main/java/com/hohltier/decompiler/views/AboutView.java
+++ src/main/java/com/hohltier/decompiler/views/AboutView.java
@@ -1,5 +1,7 @@
 package com.hohltier.decompiler.views;
 
+import com.formdev.flatlaf.FlatClientProperties;
+import com.hohltier.decompiler.controllers.WindowController;
 import com.hohltier.decompiler.services.ResourceService;
 import net.miginfocom.swing.MigLayout;
 import javax.swing.*;
@@ -9,6 +11,12 @@ import java.time.Year;
 public class AboutView extends JDialog {
 
     public AboutView() {
+        super((JFrame) WindowController.getINSTANCE().getComponent());
+        getRootPane().putClientProperty(FlatClientProperties.TITLE_BAR_SHOW_ICON, false);
+        setTitle(ResourceService.getTranslation("about"));
+        setModal(true);
+        setResizable(false);
+
         JPanel root = new JPanel(new MigLayout());
         setContentPane(root);
 
@@ -21,9 +29,6 @@ public class AboutView extends JDialog {
 
         root.add(new JLabel(String.format("\u00a9 2022 - %s Sota", Year.now().getValue())), "wrap");
 
-        setTitle(ResourceService.getTranslation("about"));
-        setModal(true);
-        setResizable(false);
         pack();
         setLocationRelativeTo(getOwner());
     }
