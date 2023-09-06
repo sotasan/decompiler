36badec5d7e28c68585f9012b91663876071cd62
Sōta <sota@sotasan.com>
2023-09-06 19:17:22 +0200

    Update font loading

--- src/main/java/com/sotasan/decompiler/Main.java
+++ src/main/java/com/sotasan/decompiler/Main.java
@@ -3,16 +3,14 @@ package com.sotasan.decompiler;
 import com.formdev.flatlaf.FlatDarkLaf;
 import com.formdev.flatlaf.FlatLaf;
 import com.formdev.flatlaf.extras.FlatInspector;
+import com.formdev.flatlaf.fonts.inter.FlatInterFont;
+import com.formdev.flatlaf.fonts.jetbrains_mono.FlatJetBrainsMonoFont;
 import com.sotasan.decompiler.controllers.WindowController;
 import com.sotasan.decompiler.services.LoaderService;
-import io.github.classgraph.ClassGraph;
-import io.github.classgraph.Resource;
 import lombok.SneakyThrows;
 import lombok.experimental.UtilityClass;
 import org.jetbrains.annotations.NotNull;
-import java.awt.*;
 import java.io.File;
-import java.util.Objects;
 
 @UtilityClass
 public class Main {
@@ -23,9 +21,9 @@ public class Main {
         System.setProperty("apple.awt.application.name", "Decompiler");
         System.setProperty("apple.awt.application.appearance", "NSAppearanceNameDarkAqua");
 
-        for (Resource font : new ClassGraph().acceptPaths("fonts").scan().getResourcesWithExtension("ttf"))
-            GraphicsEnvironment.getLocalGraphicsEnvironment().registerFont(Font.createFont(Font.PLAIN, Objects.requireNonNull(Main.class.getClassLoader().getResourceAsStream(font.getPath()))));
-
+        FlatInterFont.install();
+        FlatJetBrainsMonoFont.install();
+        FlatLaf.setPreferredFontFamily(FlatInterFont.FAMILY);
         FlatLaf.registerCustomDefaultsSource("themes");
         FlatDarkLaf.setup();
         FlatInspector.install("meta shift I");
