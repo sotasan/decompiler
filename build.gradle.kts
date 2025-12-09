3fa3cc2037390e64040374e8133f81c860c87b0b
Sōta <sota@sota.moe>
2025-12-09 20:19:31 +0100

    feat: add kotlin dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -36,11 +36,14 @@ dependencies {
     implementation("com.formdev:flatlaf-extras:3.7")
     implementation("com.formdev:flatlaf-fonts-inter:4.1")
     implementation("com.formdev:flatlaf-fonts-jetbrains-mono:2.304")
+    implementation("com.h2database:h2:2.4.240")
     implementation("com.github.java-decompiler:jd-core:v1.1.3")
     implementation("com.miglayout:miglayout-swing:11.4.2")
+    implementation("io.insert-koin:koin-core:4.1.1")
     implementation("net.fabricmc:cfr:0.2.2")
     implementation("org.bitbucket.mstrobel:procyon-compilertools:0.6.0")
     implementation("org.jetbrains:annotations:26.0.2-1")
+    implementation("org.ktorm:ktorm-core:4.1.1")
     implementation("org.vineflower:vineflower:1.11.2")
 }
 
