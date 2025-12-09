3ef2c83e829ee6f862d2a09c12afc3f665763323
Sōta <sota@sota.moe>
2025-12-09 19:16:55 +0100

    chore: update dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -31,17 +31,17 @@ repositories {
 }
 
 dependencies {
-    implementation("com.fifesoft:rsyntaxtextarea:3.5.4")
-    implementation("com.formdev:flatlaf:3.5.4")
-    implementation("com.formdev:flatlaf-extras:3.5.4")
-    implementation("com.formdev:flatlaf-fonts-inter:4.0")
+    implementation("com.fifesoft:rsyntaxtextarea:3.6.0")
+    implementation("com.formdev:flatlaf:3.7")
+    implementation("com.formdev:flatlaf-extras:3.7")
+    implementation("com.formdev:flatlaf-fonts-inter:4.1")
     implementation("com.formdev:flatlaf-fonts-jetbrains-mono:2.304")
     implementation("com.github.java-decompiler:jd-core:v1.1.3")
     implementation("com.miglayout:miglayout-swing:11.4.2")
     implementation("net.fabricmc:cfr:0.2.2")
     implementation("org.bitbucket.mstrobel:procyon-compilertools:0.6.0")
-    implementation("org.jetbrains:annotations:26.0.2")
-    implementation("org.vineflower:vineflower:1.11.1")
+    implementation("org.jetbrains:annotations:26.0.2-1")
+    implementation("org.vineflower:vineflower:1.11.2")
 }
 
 tasks {
