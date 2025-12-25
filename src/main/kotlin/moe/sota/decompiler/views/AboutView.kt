2db29d3792a367d113d6ba3ed21163aa163a217e
Sōta <sota@sota.moe>
2025-12-25 21:03:07 +0100

    refactor: about view in kotlin

--- src/main/kotlin/moe/sota/decompiler/views/AboutView.kt
+++ src/main/kotlin/moe/sota/decompiler/views/AboutView.kt
@@ -3,14 +3,12 @@ package moe.sota.decompiler.views
 import com.formdev.flatlaf.FlatClientProperties
 import com.formdev.flatlaf.extras.components.FlatButton
 import com.formdev.flatlaf.extras.components.FlatLabel
-import lombok.SneakyThrows
 import moe.sota.decompiler.services.LanguageService
 import net.miginfocom.swing.MigLayout
 import java.awt.BorderLayout
 import java.awt.Desktop
 import java.awt.Image
 import java.awt.event.ActionEvent
-import java.awt.event.ActionListener
 import java.lang.management.ManagementFactory
 import java.net.URI
 import java.time.Year
@@ -38,75 +36,88 @@ class AboutView(
     val ok: FlatButton
 
     init {
+        isModal = true
+        isResizable = false
+        title = languageService.getString("about")
         getRootPane().putClientProperty(FlatClientProperties.TITLE_BAR_SHOW_ICON, false)
-        setModal(true)
-        setResizable(false)
-        setTitle(languageService.getString("about"))
 
-        root = JPanel()
-        root.setBorder(EmptyBorder(16, 16, 16, 16))
-        root.setLayout(BorderLayout())
+        root = JPanel().apply {
+            border = EmptyBorder(16, 16, 16, 16)
+            layout = BorderLayout()
+        }
         contentPane = root
 
-        content = JPanel()
-        content.setBorder(EmptyBorder(0, 0, 16, 0))
-        content.setLayout(MigLayout())
+        content = JPanel().apply {
+            border = EmptyBorder(0, 0, 16, 0)
+            layout = MigLayout()
+        }
         root.add(content, BorderLayout.CENTER)
 
-        logo = FlatLabel()
-        logo.setBorder(EmptyBorder(0, 0, 0, 16))
-        logo.setIcon(ImageIcon(owner.getIconImages().get(0).getScaledInstance(64, 64, Image.SCALE_SMOOTH)))
-        logo.setVerticalAlignment(JLabel.TOP)
+        logo = FlatLabel().apply {
+            border = EmptyBorder(0, 0, 0, 16)
+            icon = ImageIcon(owner.getIconImages()[0].getScaledInstance(64, 64, Image.SCALE_SMOOTH))
+            verticalAlignment = JLabel.TOP
+        }
         content.add(logo, "dock west")
 
-        header = FlatLabel()
-        header.styleClass = "h1"
-        header.setText("Decompiler")
+        header = FlatLabel().apply {
+            styleClass = "h1"
+            text = "Decompiler"
+        }
         content.add(header, "wrap")
 
-        val properties = Properties()
-        properties.load(javaClass.getClassLoader().getResourceAsStream("application.properties"))
-        version = FlatLabel()
-        version.setText(String.format(languageService.getString("about.version"), properties.getProperty("version")))
+        version = FlatLabel().apply {
+            val properties = Properties()
+            properties.load(javaClass.classLoader.getResourceAsStream("application.properties"))
+            text = String.format(languageService.getString("about.version"), properties.getProperty("version"))
+        }
         content.add(version, "wrap")
 
-        copyright = FlatLabel()
-        copyright.setText(String.format("\u00a9 2022 - %s S\u014Dta", Year.now().value))
+        copyright = FlatLabel().apply {
+            text = String.format("\u00a9 2022 - %s S\u014Dta", Year.now().value)
+        }
         content.add(copyright, "wrap")
 
-        vm = JPanel()
-        vm.setBorder(BorderFactory.createTitledBorder(languageService.getString("about.vm")))
-        vm.setLayout(MigLayout())
+        vm = JPanel().apply {
+            border = BorderFactory.createTitledBorder(languageService.getString("about.vm"))
+            layout = MigLayout()
+        }
         content.add(vm, "wrap, gapy 16px")
 
-        vmName = FlatLabel()
-        vmName.setText(ManagementFactory.getRuntimeMXBean().vmName)
+        vmName = FlatLabel().apply {
+            text = ManagementFactory.getRuntimeMXBean().vmName
+        }
         vm.add(vmName, "wrap")
 
-        vmVendor = FlatLabel()
-        vmVendor.setText(ManagementFactory.getRuntimeMXBean().vmVendor)
+        vmVendor = FlatLabel().apply {
+            text = ManagementFactory.getRuntimeMXBean().vmVendor
+        }
         vm.add(vmVendor, "wrap")
 
-        vmVersion = FlatLabel()
-        vmVersion.setText(ManagementFactory.getRuntimeMXBean().vmVersion)
+        vmVersion = FlatLabel().apply {
+            text = ManagementFactory.getRuntimeMXBean().vmVersion
+        }
         vm.add(vmVersion, "wrap")
 
-        controls = JPanel()
-        controls.add(Box.createHorizontalGlue())
+        controls = JPanel().apply {
+            add(Box.createHorizontalGlue())
+        }
         controls.setLayout(BoxLayout(controls, BoxLayout.X_AXIS))
         root.add(controls, BorderLayout.SOUTH)
 
-        github = FlatButton()
-        github.addActionListener(ActionListener { event: ActionEvent? -> this.onGitHubAction(event) })
-        github.setFocusable(false)
-        github.setText("GitHub")
+        github = FlatButton().apply {
+            isFocusable = false
+            text = "GitHub"
+            addActionListener(::onGitHubAction)
+        }
         controls.add(github)
 
         controls.add(Box.createHorizontalStrut(8))
 
-        ok = FlatButton()
-        ok.addActionListener(ActionListener { event: ActionEvent? -> this.onOkAction(event) })
-        ok.setText(languageService.getString("about.ok"))
+        ok = FlatButton().apply {
+            text = languageService.getString("about.ok")
+            addActionListener(::onOkAction)
+        }
         controls.add(ok)
         getRootPane().setDefaultButton(ok)
 
@@ -114,13 +125,9 @@ class AboutView(
         setLocationRelativeTo(owner)
     }
 
-    @SneakyThrows
     private fun onGitHubAction(event: ActionEvent?) {
-        if (Desktop.isDesktopSupported() && Desktop.getDesktop()
-                .isSupported(Desktop.Action.BROWSE)
-        ) Desktop.getDesktop().browse(
-            URI("https://github.com/sotasan/decompiler")
-        )
+        if (Desktop.isDesktopSupported() && Desktop.getDesktop().isSupported(Desktop.Action.BROWSE))
+            Desktop.getDesktop().browse(URI("https://github.com/sotasan/decompiler"))
     }
 
     private fun onOkAction(event: ActionEvent?) {
