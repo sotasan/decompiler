366f7633d5eb434a21a411aa19410cded80530c1
Sōta <sota@sota.moe>
2026-07-17 16:36:47 +0200

    build: bump demo to java 26

--- demo/build.gradle.kts
+++ demo/build.gradle.kts
@@ -3,13 +3,10 @@ plugins {
     alias(libs.plugins.spotless)
 }
 
-java {
-    sourceCompatibility = JavaVersion.VERSION_17
-    targetCompatibility = JavaVersion.VERSION_17
-}
-
 spotless {
     java {
         palantirJavaFormat()
     }
 }
+
+tasks.compileJava { options.release = 26 }
